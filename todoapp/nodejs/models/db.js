69b4d48de3f41058c19d67c960c05d49456b7fb1
Student User <student@workstation.lab.example.com>
2022-02-03 13:42:42 -0500

    Fixed Express release

--- todoapp/nodejs/models/db.js
+++ todoapp/nodejs/models/db.js
@@ -4,7 +4,7 @@ module.exports.params = {
   username: process.env.MYSQL_ENV_MYSQL_USER,
   password: process.env.MYSQL_ENV_MYSQL_PASSWORD,
   params: {
-      host: "mysql",
+      host: "172.25.250.9",
       port: "3306",
       dialect: 'mysql'
   }
